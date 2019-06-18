e0bbfa1ed2bb257ef8c14f4f6e69c79f1289d863
Tot-AI <51962147+Tot-AI@users.noreply.github.com>
2019-06-18 20:06:35 +0300

    Create class NeuralConnection

---
+++ NeuronLib/NeuralСonnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuronLib
+{
+    public class NeuralСonnection
+    {
+        public Neuron Parent { get; set; }
+        public Neuron Heir { get; set; }
+    }
+}
