22171f16659dab3a0fbb0c17e4e48b0f70d2f395
Tot-AI <51962147+Tot-AI@users.noreply.github.com>
2019-06-18 19:49:59 +0300

    Create class Point3D

---
+++ NeuronLib/Point3D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuronLib
+{
+    public class Point3D
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public Point3D()
+        {
+            X = 0;
+            Y = 0;
+            Z = 0;
+        }
+        public Point3D(int X, int Y, int Z)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z;
+        }
+    }
+}
